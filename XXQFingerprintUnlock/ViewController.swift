2351efd2a9bb003213624ed8efae2420cd292514
xiaxiangquan <xiaxiangquan@dyrs.com.cn>
2016-06-08 14:57:55 +0800

    Initial Commit

---
+++ XXQFingerprintUnlock/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XXQFingerprintUnlock
+//
+//  Created by 夏祥全 on 16/6/8.
+//  Copyright © 2016年 xiaxiangquan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
